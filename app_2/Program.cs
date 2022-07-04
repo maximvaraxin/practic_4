1ff8197930c8aae0c39063ed9b09758feb7443ef
Максим Вараксин <maks-02@yandex.ru>
2022-07-04 23:10:56 +0300

    add app_2

--- app_2/Program.cs
+++ app_2/Program.cs
@@ -3,18 +3,23 @@
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine(Quantity(12));
+        {   
+            Console.Write($"Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"Количество цифр в числе: ");
+            Console.WriteLine(Quantity( number ));
+
         }
 
+        // возвращает количество чисел
         static int Quantity(int number)
         {   
-            int res = 0;
             int count = 0;
 
-            while (number != 0)
+            while (number > 0)
             {   
-                res =number / 10;
+                number = number / 10;
                 count++;
             }
 
